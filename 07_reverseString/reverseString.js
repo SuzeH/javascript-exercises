9e0151ddb76d3910c3feeb23be292c222dbd0cef
SuzeH <10055032+SuzeH@users.noreply.github.com>
2025-10-21 19:23:10 +1100

    Add split, reverse and join

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,6 +1,13 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    const letterArray = string.split('');
+    const reversedArray = letterArray.reverse();
+    const reversedString = reversedArray.join('');
+    
+    return reversedString;
 };
+// HA. or more succinctly:
+// const reverseString = string => string.split('').reverse().join('');
 
+reverseString('hello there') // returns 'ereht olleh'
 // Do not edit below this line
 module.exports = reverseString;
