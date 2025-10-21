205f3af4d85a75b67fbef50e60617659b5dcbae3
SuzeH <10055032+SuzeH@users.noreply.github.com>
2025-10-21 18:56:39 +1100

    Add loop, join strings

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,6 +1,16 @@
-const repeatString = function() {
-
+const repeatString = function (string, num) {
+if (num < 0) {
+    return 'ERROR';
+  } else {
+    let joinedString = "";
+    for (let i = 0; i < num; i++) {
+        joinedString += string;
+    }
+  return joinedString;
+}
 };
+
+repeatString('hey', 3) // returns 'heyheyhey'
 
 // Do not edit below this line
 module.exports = repeatString;
